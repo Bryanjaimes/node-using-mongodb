ccf443688e5956908f10698da154b39500b4e69e
Bryan Jaimes <bfjaimes06@gmail.com>
2021-01-21 14:34:07 -0500

    Completed Module 2

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+// connection to mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 
 app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
+    res.send(`Hi, Store server running on port ${PORT}`)
 );
 
 app.listen(PORT, () => 
