1452689eb3727cdc8cc63e48868266ff3f6cc12a
Bryan Jaimes <bfjaimes06@gmail.com>
2021-01-22 11:09:37 -0500

    Completed Module 5

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -34,7 +34,7 @@ export const getProductWithID = (req, res) => {
 }
 
 export const updateProduct = (req, res) => {
-    Product.findOneAndUpdate({_id:req.params.ProductID}, req.body, { new: true, useFindAndModify: false }, (err, Product) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product) => {
         if (err){
             res.send(err)
         }
@@ -43,7 +43,7 @@ export const updateProduct = (req, res) => {
 }
 
 export const deleteProduct = (req, res) => {
-    Product.deleteOne({ _id: req.params.ProductID }, (err, Product) => {
+    Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {
         if (err){
             res.send(err)
         }
