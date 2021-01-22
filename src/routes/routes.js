d3c9b5c37fe9e7cf79c6296125d0e66dcd76d883
Bryan Jaimes <bfjaimes06@gmail.com>
2021-01-22 10:46:23 -0500

    Completed Module 3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
-import { addNewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
 
+    // Get endpoint 
+    .get(getProducts)
     // Post endpoint
     .post(addnewProduct);
+
+    app.route('/products/:ProductID')
+
+    .get(getProductWithID)
 }
 
 export default routes;
